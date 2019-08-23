91ac419fe17d7ed2d06c3fb962f5a8be9457cf6f
Kasio <kasioeduardo13@gmail.com>
2019-08-23 15:27:52 -0300

    fix: session controler validation

--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -7,12 +7,16 @@ import authConfig from '../../config/auth';
 
 class SessionController {
   async store(req, res) {
-    const shema = Yup.object().shape({
+    const schema = Yup.object().shape({
       email: Yup.string()
         .email()
         .required(),
       password: Yup.string().required(),
     });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation Fails' });
+    }
     const { email, password } = req.body;
 
     const user = await User.findOne({ where: { email } });
