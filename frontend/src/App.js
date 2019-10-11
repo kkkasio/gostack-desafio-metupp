4f736ec1f35ebaadc433c075967149ca537e5d2a
Kásio Eduardo <kasioeduardo13@gmail.com>
2019-10-11 12:25:21 -0300

    Estilo global

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Router } from 'react-router-dom';
 import history from '~/services/history';
 import '~/config/ReactotronConfig';
+import GlobalStyle from '~/styles/global';
 
 import Routes from '~/routes';
 
 function App() {
   return (
     <Router history={history}>
+      <GlobalStyle />
       <Routes />
     </Router>
   );
---
+++ frontend/src/styles/global.js
@@ -0,0 +1,42 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Lato:400,700&display=swap');
+
+* {
+  margin:0;
+  padding:0;
+  box-sizing:border-box;
+  outline:none;
+}
+
+*:focus {
+outline: none;
+}
+
+
+html,body,#root {
+  height:100%;
+}
+body {
+  -webkit-font-smoothing: antialiased;
+}
+
+body,input,button {
+  font:14px 'Lato', sans-serif;
+}
+
+a{
+  text-decoration:none;
+}
+
+ul{
+  list-style:none;
+}
+
+button{
+  cursor:pointer;
+}
+
+
+`;
